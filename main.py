b3a09d89a78530eb1f692b4f42f7494a00b43dc3
HectorMilla <37377168+HectorMilla@users.noreply.github.com>
2024-01-25 21:38:34 -0500

    get character count will now sort list of characters by word count, formatted output in main function

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ def main():
     path = "books/frankenstein.txt"
     text = get_book_text(path)
     word_count = get_word_count(text)
-    print(text)
-    print (word_count)
+    character_count = get_character_count(text)
+    print(f"--- Begin report of {path}")
+    print (f"{word_count}\n")
+    for character in character_count:
+        if character[0].isalpha():
+            print(f"The '{character[0]}' character was found {character[1]} times")
+    print("\n--- End report ---")
+    
+
+
+    
 
 def get_book_text(path):
     with open(path) as f:
@@ -12,7 +21,21 @@ def get_book_text(path):
 def get_word_count(text):
     words = text.split()
     count = len(words)
-    return count
+    return f"{count} words were found in the document"
+
+def get_character_count(text):
+    characters = {}
+    for character in text.lower():
+        if character not in characters:
+            characters[character] = 1
+        else:
+            characters[character] += 1
+    characters = list(characters.items())
+    characters.sort(reverse= True, key=lambda a: a[1])
+    return characters
+    
+
+
 main()
 
 
