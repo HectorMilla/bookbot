0fb6ead592afcc4f4f17fecf121329d454190f60
hectormilla <37377168+HectorMilla@users.noreply.github.com>
2024-07-30 22:42:03 -0400

    added function to create report of data found

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 def main():
    book_path = "books/frankenstein.txt"
    text = get_book_text(book_path)
-   wordcount = get_word_count
+   wordcount = get_word_count(text)
    char_list = get_char_count(text)
-   print(char_list)
+   sorted_char_list = get_sorted_char_list(char_list)
+   print(f"-- Begin report of {book_path} ---")
+   print(f"{wordcount} words foind in the document/n/n")
+   for char in sorted_char_list:
+      if not char['char'].isalpha():
+         continue
+      else:
+         print(f"The '{char['char']}' character was found {char['num']} times")
+
+
 def get_book_text(path):
    with open(path) as f:
       return f.read()
@@ -21,6 +30,14 @@ def get_char_count(text):
         else:
             char_list[char] = 1
     return char_list
-    
-    
+
+def sort_on(d):
+    return d["num"]
+
+def get_sorted_char_list(dict):
+    sorted_list = []
+    for char in dict:
+        sorted_list.append({'char': char, "num": dict[char]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 main()
